f4cecb5ba4f6aa020ecb57e4702089d2cf779889
Siraaj.Bedford <Siraaj.Bedford@github.com>
2022-06-28 19:24:50 +0200

    Initial commit

---
+++ src/auto_sld.py
@@ -0,0 +1,17 @@
+# import libraries
+# TODO single line diagrams for one lighting circuit from load sheet
+
+
+
+# get criteria for single line diagrams
+# process lighting requirement to nearest breaker (upward)
+
+
+
+# process criteria to single line diagrams
+# Convert to tree table
+
+
+
+# write to single line diagram on a pdf
+# TODO write to a pdf
